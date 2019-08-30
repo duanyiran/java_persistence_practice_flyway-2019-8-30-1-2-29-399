22cb8c5fb51b3c4e0102dacb0c9c726748f12868
duanyiran <dyr6@qq.com>
2019-08-30 18:29:40 +0800

    day10

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,7 @@
+create table USER (
+ ID varchar(100),
+ USERNAME varchar(100),
+ PASSWORD varchar(100),
+ PASSWORD_EXPIRED varchar(100),
+ ROLE varchar(100),
+ CREATE_TIME TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
